16e4c082a63ee95b0cfd66e6592c7bc04fc605ec
georgetoloraia <gtoloraia19@gmail.com>
2023-11-14 01:51:32 +0400

    add Company Model - [] in models.py add Company - [] in models.py Complex add company Foreignkey Company - [] in serializers.py add CompanySerializer - [] in views.py add CompanyListCreateView and CompanyDetailView - [] in urls.py add companies/ for add company - [] in urls.py add companies/<int:pk>/ for view company

--- complex/models.py
+++ complex/models.py
@@ -1,8 +1,47 @@
 from django.db import models
 
 
+'''
+    The Company's Model
+'''
+class Company(models.Model):
+    ''' კომპანიის სახელი ( დასახელება )'''
+    name = models.CharField(max_length=255)
+
+    ''' კომპანიის მისამართი. მაგ: ილია ჭავჭავაძის გამზირი # 36 
+    '''
+    address = models.TextField()
+    
+    ''' მობილური ტელეფონოის ნომერი, მაგ: 595456545'''
+    Mobile = models.CharField(max_length=20)
+
+    ''' სახლის ტელეფონი. მაგ: 03222654854'''
+    Mobile_Home = models.CharField(max_length=20)
+
+    ''' ცომპანიის მაილის მისამართი'''
+    email = models.EmailField()
+
+    ''' კომპანიის ვებ გვერდის მისამართი (ლინკი)'''
+    companyweb = models.URLField(max_length=200, blank=True, null=True)
+
+    ''' კომპანიის შესახებ ტექსტური ინფორმაცია'''
+    aboutcompany = models.TextField(blank=True, null=True)
+
+    ''' კომპანიის ლოგო'''
+    logocompany = models.ImageField(upload_to='company_logos/', blank=True, null=True)
+
+
+    def __str__(self):
+        return self.name
+
+
 
 class Complex(models.Model):
+    ''' This ForeignKey establishes a many-to-one relationship between the Complex and Company models
+    indicating that each Complex is associated with one Company
+    and each Company can be associated with multiple Complex instances '''
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
     # complex_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     address = models.TextField()
