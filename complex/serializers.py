b896c7f07c19ed63d76a5143eb69a92015df395c
georgetoloraia <gtoloraia19@gmail.com>
2023-11-17 21:38:27 +0400

    add Lists

--- complex/serializers.py
+++ complex/serializers.py
@@ -2,10 +2,6 @@ from rest_framework import serializers
 from .models import Complex , ComplexImage, Company, Apartment
 
 
-class CompanySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Company
-        fields = '__all__'
 
 class ComplexImageSerializer(serializers.ModelSerializer):
     class Meta:
@@ -20,6 +16,30 @@ class ApartmentSerializer(serializers.ModelSerializer):
 
 class ComplexSerializer(serializers.ModelSerializer):
     images = ComplexImageSerializer(many=True, read_only=True)
+    apartments = serializers.SerializerMethodField()
     class Meta:
         model = Complex
         fields = '__all__'
+
+    def get_apartments(self, obj):
+        return list(obj.apartments.values_list('id', flat=True))
+
+
+
+class CompanySerializer(serializers.ModelSerializer):
+    complexes = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Company
+        fields = '__all__'
+
+    def get_complexes(self, obj):
+        return list(obj.complexes.values_list('id', flat=True))
+
+
+
+
+
+
+
+
