ee8bcc45150fd4cd77d25bdfbd63ce360cd39071
nodarisvanidze88 <nodarisvanidze88@gmail.com>
2023-11-23 11:11:22 +0400

    model viws

--- complex/views.py
+++ complex/views.py
@@ -1,13 +1,34 @@
 from rest_framework import viewsets
 from django_filters.rest_framework import DjangoFilterBackend
-from .models import Complex, Apartment, Company, ComplexImage
-from .serializers import ComplexSerializer, ApartmentSerializer, CompanySerializer, ComplexImageSerializer
+from .models import Complex, Apartment, Company, ComplexImage, GeoCities, GeoUbani, District, DirectAddress
+from .serializers import ComplexSerializer, ApartmentSerializer, CompanySerializer, ComplexImageSerializer,DirectAddressSerializer,DistrictSerializer,GeoCitiesSerializer,GeoUbaniSerializer
 from .filters import ComplexFilter, ApartmentFilter
 from rest_framework.pagination import LimitOffsetPagination
 
 class CustomLimitOffsetPagination(LimitOffsetPagination):
     default_limit = 10
     max_limit = 100
+
+class GeoCitiesViewset(viewsets.ModelViewSet):
+    queryset = GeoCities.objects.all()
+    serializer_class = GeoCitiesSerializer
+    pagination_class = CustomLimitOffsetPagination
+
+class GeoUbaniViewset(viewsets.ModelViewSet):
+    queryset = GeoUbani.objects.all()
+    serializer_class = GeoUbaniSerializer
+    pagination_class = CustomLimitOffsetPagination
+
+class DistrictViewset(viewsets.ModelViewSet):
+    queryset = District.objects.all()
+    serializer_class = DistrictSerializer
+    pagination_class = CustomLimitOffsetPagination
+
+class DirectAddressViewset(viewsets.ModelViewSet):
+    queryset = DirectAddress.objects.all()
+    serializer_class = DirectAddressSerializer
+    pagination_class = CustomLimitOffsetPagination
+
 
 class ComplexViewSet(viewsets.ModelViewSet):
     queryset = Complex.objects.all()
