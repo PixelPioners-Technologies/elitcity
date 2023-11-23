7520f26dc2037a1fe6ca444d70dc0eee68b26db7
nodarisvanidze88 <nodarisvanidze88@gmail.com>
2023-11-23 11:12:13 +0400

    model urls

--- complex/urls.py
+++ complex/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path , include
 from rest_framework.routers import DefaultRouter
-from .views import ComplexViewSet , ComplexImageViewSet, CompanyViewSet, ApartmentViewSet
+from .views import ComplexViewSet , ComplexImageViewSet, CompanyViewSet, ApartmentViewSet,DirectAddressViewset,GeoUbaniViewset,DistrictViewset,GeoCitiesViewset
 
 
 router = DefaultRouter()
@@ -8,6 +8,10 @@ router.register(r'complex' , ComplexViewSet , basename='complex')
 router.register(r'companies' , CompanyViewSet , basename='companies' ) 
 router.register(r'images' ,  ComplexImageViewSet , basename='images' ) 
 router.register(r'apartments', ApartmentViewSet)
+router.register(r'geocutues', GeoCitiesViewset)
+router.register(r'geoubani', GeoUbaniViewset)
+router.register(r'district', DistrictViewset)
+router.register(r'directaddress', DirectAddressViewset)
 
 
 urlpatterns = [
