d6ef68b594997e4740dfbeefd5bbc3925fb82cd8
nodarisvanidze88 <nodarisvanidze88@gmail.com>
2023-11-23 11:10:01 +0400

    new structure serializers

--- complex/serializers.py
+++ complex/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Complex , ComplexImage, Company, Apartment
+from .models import Complex , ComplexImage, Company, Apartment, GeoCities, GeoUbani, District, DirectAddress
 
 
 class CompanySerializer(serializers.ModelSerializer):
@@ -17,9 +17,31 @@ class ApartmentSerializer(serializers.ModelSerializer):
         model = Apartment
         fields = '__all__'
 
+class DirectAddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DirectAddress
+        fields = '__all__'
+
+class DistrictSerializer(serializers.ModelSerializer):
+    street = DirectAddressSerializer(read_only=True)
+    class Meta:
+        model = District
+        fields = '__all__'
+
+class GeoUbaniSerializer(serializers.ModelSerializer):
+    district = DistrictSerializer(read_only=True)
+    class Meta:
+        model = GeoUbani
+        fields = '__all__'
+class GeoCitiesSerializer(serializers.ModelSerializer):
+    ubani = GeoUbaniSerializer(read_only=True)
+    class Meta:
+        model = GeoCities
+        fields = '__all__'
 
 class ComplexSerializer(serializers.ModelSerializer):
     images = ComplexImageSerializer(many=True, read_only=True)
+    address = DirectAddressSerializer()
     class Meta:
         model = Complex
         fields = '__all__'
